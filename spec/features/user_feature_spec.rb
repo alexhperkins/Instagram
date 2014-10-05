20e721c7fc7512ecba19c26e99c5c1b6ad0b72a3
byverdu <byverdu@gmail.com>
2014-10-05 22:21:38 +0100

    added picture controller and tested index.html

---
+++ spec/features/user_feature_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+describe "pictures" do
+  
+  context "No pictures have been added" do 
+
+  	it "should display a link to add a picture" do
+  		
+  		visit '/pictures'
+
+  		expect(page).to have_content('No pictures yet')
+  		expect(page).to have_link('Add a picture')	
+  	end
+  end
+
+end
