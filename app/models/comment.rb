a80190c2ace2ee81e04eded7a813d5d7e2757000
byverdu <byverdu@gmail.com>
2014-10-06 02:09:29 +0100

    validates rating between 1..5, tested

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
 	belongs_to :restaurant
 
 	validates :content, length: {minimum:1}
+	validates  :rating,   inclusion: (1..5)
 end
--- spec/models/comment_model_spec.rb
+++ spec/models/comment_model_spec.rb
@@ -10,5 +10,21 @@ RSpec.describe Comment, type: :model do
 		expect(comment).not_to be_valid
 	end
 
+	it "is not valid with a rating less than 1" do
+    comment = Comment.new(rating: '-1')
+
+    expect(comment).to have(1).error_on(:rating)
+	end
+
+	it "is not valid with a rating more than 5" do
+    comment = Comment.new(rating: '6')
+
+    expect(comment).to have(1).error_on(:rating)
+	end
+
+	it "is only valid with a rating between 1..5" do
+		comment = Comment.new(rating:4, content: 'Best picture ever')
+		expect(comment).to be_valid
+	end
 
 end
